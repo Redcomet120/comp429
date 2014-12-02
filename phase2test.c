43e6f35d231c5aa1b618fe3ab1bc72db86897003
AllTimePlumber <superbros2@gmail.com>
2014-12-01 16:17:46 -0800

    Phase 2 Test

---
+++ phase2test.c
@@ -0,0 +1,152 @@
+/*
+    Raw UDP sockets
+    Silver Moon (m00n.silv3r@gmail.com)
+*/
+#include<stdio.h> //for printf
+#include<string.h> //memset
+#include<sys/socket.h>    //for socket ofcourse
+#include<stdlib.h> //for exit(0);
+#include<errno.h> //For errno - the error number
+#include<netinet/udp.h>   //Provides declarations for udp header
+#include<netinet/ip.h>    //Provides declarations for ip header
+#include <netinet/ip_icmp.h>
+#include <unistd.h> 
+/*
+    96 bit (12 bytes) pseudo header needed for udp header checksum calculation
+*/
+struct pseudo_header
+{
+    u_int32_t source_address;
+    u_int32_t dest_address;
+    u_int8_t placeholder;
+    u_int8_t protocol;
+    u_int16_t udp_length;
+};
+ 
+/*
+    Generic checksum calculation function
+*/
+unsigned short csum(unsigned short *ptr,int nbytes)
+{
+    register long sum;
+    unsigned short oddbyte;
+    register short answer;
+ 
+    sum=0;
+    while(nbytes>1) {
+        sum+=*ptr++;
+        nbytes-=2;
+    }
+    if(nbytes==1) {
+        oddbyte=0;
+        *((u_char*)&oddbyte)=*(u_char*)ptr;
+        sum+=oddbyte;
+    }
+ 
+    sum = (sum>>16)+(sum & 0xffff);
+    sum = sum + (sum>>16);
+    answer=(short)~sum;
+     
+    return(answer);
+}
+ 
+int main (void)
+{
+    int rc = -1;
+    char rbuf[sizeof(struct iphdr) + sizeof(struct icmp)];  
+    struct sockaddr_in raddr;
+    socklen_t raddr_len;
+    //Create a raw socket of type IPPROTO
+    int s = socket (AF_INET, SOCK_RAW, IPPROTO_RAW);
+     
+    if(s == -1)
+    {
+        //socket creation failed, may be because of non-root privileges
+        perror("Failed to create raw socket");
+        exit(1);
+    }
+    int t = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
+    if(t == -1)
+    {
+        //socket creation failed, may be because of non-root privileges
+        perror("Failed to create receive raw socket");
+        exit(1);
+    }
+
+     
+    //Datagram to represent the packet
+    char datagram[4096] , source_ip[32] , *data , *pseudogram;
+     
+    //zero out the packet buffer
+    memset (datagram, 0, 4096);
+     
+    //IP header
+    struct iphdr *iph = (struct iphdr *) datagram;
+     
+    //UDP header
+    struct udphdr *udph = (struct udphdr *) (datagram + sizeof (struct ip));
+     
+    struct sockaddr_in sin;
+    struct pseudo_header psh;
+     
+    //Data part
+    data = datagram + sizeof(struct iphdr) + sizeof(struct udphdr);
+    strcpy(data , "THISISATEST");
+     
+    //some address resolution
+    strcpy(source_ip , "192.168.1.2");
+     
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(80);
+    sin.sin_addr.s_addr = inet_addr ("4.2.2.2");
+     
+    //Fill in the IP Header
+    iph->ihl = 5;
+    iph->version = 4;
+    iph->tos = 0;
+    iph->tot_len = sizeof (struct iphdr) + sizeof (struct udphdr) + strlen(data);
+    iph->id = htonl (54321); //Id of this packet
+    iph->frag_off = 0;
+    iph->ttl = 255;
+    iph->protocol = IPPROTO_UDP;
+    iph->check = 0;      //Set to 0 before calculating checksum
+    iph->saddr = inet_addr ( source_ip );    //Spoof the source ip address
+    iph->daddr = sin.sin_addr.s_addr;
+     
+    //Ip checksum
+    iph->check = csum ((unsigned short *) datagram, iph->tot_len);
+     
+    //UDP header
+    udph->source = htons (6666);
+    udph->dest = htons (9876);
+    udph->len = htons(8 + strlen(data)); //tcp header size
+    udph->check = 0; //leave checksum 0 now, filled later by pseudo header
+         
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        //Send the packet
+        if (sendto (s, datagram, iph->tot_len ,  0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+        {
+            perror("sendto failed");
+        }
+        //Data send successfully
+        else
+        {
+            printf ("Packet Send. Length : %d \n" , iph->tot_len);
+        }
+        close(s);
+    }
+    else
+    {
+        rc = recvfrom(t, rbuf, sizeof(rbuf) ,  0, (struct sockaddr *)&raddr, &raddr_len); 
+        struct iphdr* iphdr = NULL;
+        struct icmphdr* icmphdr = NULL;
+        iphdr = (struct iphdr*)rbuf;
+        icmphdr = (struct icmphdr*)(rbuf + (iphdr->ihl * 4));
+        printf("ICMP code: %u\n ICMP type%d\n",icmphdr->code,icmphdr->type);
+        close(t);
+    } 
+    return 0;
+}
